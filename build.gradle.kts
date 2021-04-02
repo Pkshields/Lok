647e1f7e91e763ec6ab3ea134451271b4ce83879
Paul Shields <pkshields@gmail.com>
2021-04-02 21:40:40 +0100

    Adding ktlint and detekt for some static analysis.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,34 @@
-plugins {
-    kotlin("jvm") version "1.4.32"
-}
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 group = "dev.paulshields"
 version = "1.0-SNAPSHOT"
 
+plugins {
+    kotlin("jvm") version "1.4.32"
+
+    id("io.gitlab.arturbosch.detekt").version("1.16.0-RC1")
+    id("org.jlleitschuh.gradle.ktlint").version("10.0.0")
+}
+
 repositories {
     mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+configure<KtlintExtension> {
+    verbose.set(true)
+}
+
+detekt {
+    config = files("detekt-config.yml")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 dependencies {
