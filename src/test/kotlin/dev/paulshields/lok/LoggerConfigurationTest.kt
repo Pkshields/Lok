f1a7c1b6dd10790dc79f0910ce2a4e892830139f
Paul Shields <pkshields@gmail.com>
2021-11-19 23:17:04 +0000

    Adding a little information for future generations.

--- src/test/kotlin/dev/paulshields/lok/LoggerConfigurationTest.kt
+++ src/test/kotlin/dev/paulshields/lok/LoggerConfigurationTest.kt
@@ -4,6 +4,9 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
+/**
+ * NOTE: The functionality associated with the logger configuration is tested in the internal.internallogger.Configured tests
+ */
 class LoggerConfigurationTest {
     @Test
     fun `should default to debug logging`() {
